156d778d6e11ee8fd9b28783bc793d46bfb3c616
DaveDood2 <artistic109@gmail.com>
2023-03-20 19:26:04 -0400

    added basic level geometry/movement

--- character.gd
+++ character.gd
@@ -1,16 +1,63 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+"""
 extends RigidBody2D
 
 @export var health = 100
 @export var speed = 400
 @export var team = 0
-
-
+@export var jump_velocity = -400.0
+var screen_size
+var gravity
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	screen_size = get_viewport_rect().size
+	gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var velocity = Vector2.ZERO # The player's movement vector.
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	
+	if velocity.length() > 0:
+		velocity.x = velocity.x * speed
+		
+	move_and_slide()
+"""
