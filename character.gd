bccf857023cc5e43bac8a55282a6eb523d3d4ac0
DaveDood2 <artistic109@gmail.com>
2023-03-19 22:18:01 -0400

    Added squid sprite

---
+++ character.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+@export var health = 100
+@export var speed = 400
+@export var team = 0
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
